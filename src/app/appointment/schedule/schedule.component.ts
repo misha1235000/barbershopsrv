f9566fb61cab5f94cb5337934b6611509efa2d58
Misha <misha1235000@gmail.com>
2018-07-10 12:42:39 +0300

    rename css to scss

--- src/app/appointment/schedule/schedule.component.ts
+++ src/app/appointment/schedule/schedule.component.ts
@@ -77,7 +77,7 @@ function getTimeRanges(openTime: string, closeTime: string, step: number, date: 
 @Component({
   selector: 'app-schedule',
   templateUrl: './schedule.component.html',
-  styleUrls: ['./schedule.component.css']
+  styleUrls: ['./schedule.component.scss']
 })
 
 export class ScheduleComponent implements OnInit {
