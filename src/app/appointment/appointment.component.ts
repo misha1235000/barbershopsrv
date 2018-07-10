1bfcd12df90e33b4f26e1a576ceb7406f58bf404
Misha <misha1235000@gmail.com>
2018-07-10 12:54:26 +0300

    refactor appointment.component

--- src/app/appointment/appointment.component.ts
+++ src/app/appointment/appointment.component.ts
@@ -1,49 +1,63 @@
-import { Component, OnInit, Inject } from '@angular/core';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
-import { AddAppointmentComponent } from './add-appointment/add-appointment.component';
+import { Component, OnInit} from '@angular/core';
+import {MatDialog} from '@angular/material';
 
 @Component({
   selector: 'app-appointment',
   templateUrl: './appointment.component.html',
-  styleUrls: ['./appointment.component.css']
+  styleUrls: ['./appointment.component.scss']
 })
 export class AppointmentComponent implements OnInit {
   
+  /**
+   * Injects the Material Dialog.
+   * @param dialog 
+   */
   constructor(public dialog: MatDialog) { }
-  isChecked: boolean = false;
-  isFinished: boolean = false;
-  isTypes: boolean = false;
-  isFirst: boolean = true;
-  isSchedule: boolean = false;
-  selectedTab: number = 3;
+  
+  isChecked = false;
+  isFinished = false;
+  isTypes = false;
+  isFirst = true;
+  isSchedule = false;
+  selectedTab = 3;
   typesParent: any[];
   appointmentParent: any[];
 
+  /**
+   * When check event happens from the type-list.component
+   * @param isChecked 
+   */
   onCheck(isChecked) {
     this.isChecked = isChecked;
   }
 
+  /**
+   * When types are being sent from the type-list.component
+   * @param types 
+   */
   onTypeSend(types) {
     this.isTypes = true;
     this.selectedTab = 2;
     this.typesParent = types;
   }
 
+  /**
+   * When schedule is being sent from scheudle.component
+   * @param schedule 
+   */
   onScheduleSend(schedule) {
     this.isSchedule = true;
     this.selectedTab = 1;
     this.appointmentParent = schedule;
   }
 
-  ngOnInit() {
-    
-  }
+  ngOnInit() {}
 
+  /**
+   * 
+   * @param event 
+   */
   isNextDisable(event) {
-    if (event > this.selectedTab) {
-     // this.isNext = false;
-    }
-
     this.selectedTab = event;
   }
 }
