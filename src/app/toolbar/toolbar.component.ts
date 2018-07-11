7ec5c3435335955617dbf9d13c876a98145c1faa
Misha <misha1235000@gmail.com>
2018-07-11 09:53:12 +0300

    Refactor of the code in toolbar.component

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,19 +1,23 @@
+// toolbar.component
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.css']
+  styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
+  /**
+   * Removes the active of a target class if other is beign clicked.
+   * @param event
+   */
   InfoClicked(event) {
     event.target.classList.remove('active');
   }
-
 }
